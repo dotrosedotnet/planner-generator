0926f539ec1df715debe7fc47b4c8c24080130a7
dotrosedotnet <stuff@dottie.me>
2025-08-03 16:44:07 -0400

    init

---
+++ daily/page_orchestrator.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+# this document will take the settings from each sections' `settings.md`, as
+# well as the page's `settings.md` and apply them to the page parts it will
+# also lay the page parts out on the page
+
+echo 1
+
