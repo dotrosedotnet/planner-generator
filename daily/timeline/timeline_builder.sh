b06224db81ecec1605d23906eaa26d425a72fc9f
dotrosedotnet <stuff@dottie.me>
2025-08-04 21:13:42 -0400

    strict mode and clarity

--- daily/timeline/timeline_builder.sh
+++ daily/timeline/timeline_builder.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 # this file will eventually take arguments from the page orchestrator script
 # but for now, it will watch the settings and ps files and update a tmp version for viewing
 
-# Control Flow
 # make tmp folder
 # generate timeline.tmp.ps from timeline.ps and timeline_settings.md
+#   use default values in case the settings gets messed up/for making the script
 # watch timeline.ps and timeline_settings.md for changes
 # update timeline.tmp.ps
 # delete tmp folder on quit
@@ -19,3 +22,4 @@ if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
 fi
 
 mkdir tmp
+cp timeline.ps tmp/
