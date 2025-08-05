971b313f22ccfb559ff7d08b947b1198993e0a17
dotrosedotnet <stuff@dottie.me>
2025-08-04 20:33:13 -0400

    starting timeline builder

--- daily/timeline/timeline_builder.sh
+++ daily/timeline/timeline_builder.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo 1
+# this file will eventually take arguments from the page orchestrator script
+# but for now, it will watch the settings and ps files and update a tmp version for viewing
+
+# Control Flow
+# make tmp folder
+# generate timeline.tmp.ps from timeline.ps and timeline_settings.md
+# watch timeline.ps and timeline_settings.md for changes
+# update timeline.tmp.ps
+# delete tmp folder on quit
+
+grep -niIr "^#\ " timeline_settings.md
