e339e4368601010ee7c5d7c3722e53da0f1ff26e
dotrosedotnet <stuff@dottie.me>
2025-08-05 07:52:02 -0400

    cleaning and clarifying and fun formatting

--- daily/timeline/timeline_builder.sh
+++ daily/timeline/timeline_builder.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-IFS=$'\n\t'
+################################################################################
+# this file will eventually take arguments from the page orchestrator script,
+# but for now, it will watch the settings and ps files and update a tmp version
+# for viewing
+################################################################################
 
-# this file will eventually take arguments from the page orchestrator script
-# but for now, it will watch the settings and ps files and update a tmp version for viewing
+# unofficial bash strict mode
+set -euo pipefail IFS=$'\n\t'
 
-# make tmp folder
-# generate timeline.tmp.ps from timeline.ps and timeline_settings.md
-#   use default values in case the settings gets messed up/for making the script
-# watch timeline.ps and timeline_settings.md for changes
-# update timeline.tmp.ps
-# delete tmp folder on quit
+# GAMEPLAN
+# - [x] make tmp folder
+# - [ ] generate timeline.tmp.ps from timeline.ps and timeline_settings.md
+#   - [ ] use default values if there are no matches
+# - [ ] watch timeline.ps and timeline_settings.md for changes
+# - [ ] update timeline.tmp.ps
+# - [ ] delete tmp folder on quit
 
+# keep the script from running outside of it's directory
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 CURRENT_DIR=$(pwd)
 
@@ -20,6 +25,10 @@ if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
   echo "Error: This script must be run from it's location: $SCRIPT_DIR"
   exit 1
 fi
+
+##=============##
+# let's do this #
+##=============##
 
 mkdir tmp
 cp timeline.ps tmp/
